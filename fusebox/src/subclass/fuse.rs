7c17dade46292b9cd1693e9bb156b99e48fe1fd4
Lleyton Gray <lleyton@fyralabs.com>
2023-01-04 01:42:52 -0800

    Some subclassing stuffs

--- fusebox/src/subclass/fuse.rs
+++ fusebox/src/subclass/fuse.rs
@@ -1,12 +1,42 @@
 use crate::Fuse;
-use glib::subclass::prelude::*;
+use glib::{subclass::prelude::*, translate::ToGlibPtr};
 
 pub trait FuseImpl: ObjectImpl {
-  fn code_name(&self) -> String;
-  fn display_name(&self) -> String;
-  fn description(&self) -> String;
-  fn icon(&self) -> String;
-  fn get_widget(&self) -> gtk::Widget;
+    fn get_widget(&self) -> gtk::Widget;
+    fn shown(&self);
+    fn hidden(&self);
 }
 
-unsafe impl<T: FuseImpl> IsSubclassable<T> for Fuse {}
+unsafe impl<T: FuseImpl> IsSubclassable<T> for Fuse {
+    fn class_init(class: &mut glib::Class<Self>) {
+        Self::parent_class_init::<T>(class);
+
+        let klass = class.as_mut();
+        klass.get_widget = Some(get_widget::<T>);
+        klass.shown = Some(shown::<T>);
+        klass.hidden = Some(hidden::<T>);
+    }
+}
+
+unsafe extern "C" fn get_widget<T: FuseImpl>(
+    ptr: *mut ffi::FuseboxFuse,
+) -> *mut gtk::ffi::GtkWidget {
+    let instance = &*(ptr as *mut T::Instance);
+    let imp = instance.imp();
+
+    imp.get_widget().to_glib_full()
+}
+
+unsafe extern "C" fn shown<T: FuseImpl>(ptr: *mut ffi::FuseboxFuse) {
+    let instance = &*(ptr as *mut T::Instance);
+    let imp = instance.imp();
+
+    imp.shown();
+}
+
+unsafe extern "C" fn hidden<T: FuseImpl>(ptr: *mut ffi::FuseboxFuse) {
+    let instance = &*(ptr as *mut T::Instance);
+    let imp = instance.imp();
+
+    imp.hidden();
+}
