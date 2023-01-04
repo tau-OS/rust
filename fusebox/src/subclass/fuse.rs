09a3b6766e0a707582470414b054ca38fb10c61d
Lleyton Gray <lleyton@fyralabs.com>
2023-01-03 20:35:40 -0800

    Testing out fusebox subclassing

--- fusebox/src/subclass/fuse.rs
+++ fusebox/src/subclass/fuse.rs
@@ -1,6 +1,11 @@
-use crate::{Fuse, prelude::FuseExt};
+use crate::Fuse;
 use glib::subclass::prelude::*;
 
-pub trait FuseImpl: FuseExt + ObjectImpl {}
+pub trait FuseImpl: ObjectImpl {
+  fn code_name(&self) -> String;
+  fn display_name(&self) -> String;
+  fn description(&self) -> String;
+  fn icon(&self) -> String;
+}
 
 unsafe impl<T: FuseImpl> IsSubclassable<T> for Fuse {}
